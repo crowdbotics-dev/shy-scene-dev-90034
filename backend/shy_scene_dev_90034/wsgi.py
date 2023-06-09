957a271b67327dec1a08bf266151388ded2b585c
Crowdbotics <engineering@crowdbotics.com>
2023-06-09 10:23:34 +0000

    Initial commit.

---
+++ backend/shy_scene_dev_90034/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shy_scene_dev_90034 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shy_scene_dev_90034.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shy-scene-dev-90034",
+  url: "https://shy-scene-dev-90034.botics.co",
+  api: "https://shy-scene-dev-90034.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
